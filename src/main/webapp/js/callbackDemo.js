0c8f1d0b6ccddb25a1aef86d2ee20011429dd709
Trevor Lalish-Menagh <trev@trevreport.org>
2012-04-07 23:27:15 -0400

    added callback demo

---
+++ src/main/webapp/js/callbackDemo.js
@@ -0,0 +1,11 @@
+if (typeof NS === 'undefined' || !NS) {
+    var NS = {};
+}
+
+NS.runIfTrevor = function (name, callback) {
+    if (name === 'Trevor') {
+    	callback(name);
+    } else {
+    	console.log('You aren\'t Trevor');
+    }
+};
